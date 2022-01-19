7f3ba42eea54817a4d330aa73bdd077f7a17f050
Joy <90296644+Joy4620@users.noreply.github.com>
2022-01-18 20:50:15 -0800

    Add files via upload

---
+++ 2016 j4.py
@@ -0,0 +1,21 @@
+meters = 120
+clock = input().split(':')
+clock = int(clock[0])*60 + int(clock[1])
+rush_hour = [[420, 600], [900, 1140]]
+
+while meters:
+    if (clock >= rush_hour[0][0] and clock < rush_hour[0][1]) or (clock >= rush_hour[1][0] and clock < rush_hour[1][1]):
+        clock += 20
+        meters -= 10
+    else:
+        if meters < 20:
+            clock += meters
+            meters = 0
+        else:
+            meters -= 20
+            clock += 20
+
+output = f'0{str((clock//60)%24)}' if len(str((clock//60)%24)) == 1 else str((clock//60)%24)
+output += f':0{str(clock%60)}' if len(str(clock%60)) == 1 else f':{str(clock%60)}'
+print(output)
+
